628d874d614f9f32c8cba6c0a05872c1ef3d325b
Kosmik <malpa.w.klapkach@gmail.com>
2022-11-12 10:15:33 +0100

    Add main camera setter

---
+++ Assets/Scripts/Camera Fit/MainAreaSizeSetter.cs
@@ -0,0 +1,29 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class MainAreaSizeSetter : MonoBehaviour
+{
+    [SerializeField]
+    private CopyCameraSettings settings;
+    [SerializeField]
+    private CameraMainArea cameraMainArea;
+
+    private void Start()
+    {
+        Refresh();
+    }
+
+
+    [ContextMenu("Refresh")]
+    private void RefreshEditor()
+    {
+        Refresh();
+        EditorUtility.SetDirty(this);
+    }
+
+
+    private void Refresh()
+    {
+        cameraMainArea.Size = settings.WorldTileSize;
+    }
+}
