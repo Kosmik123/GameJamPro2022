e7106a97b22b32b39e3a97b86c7a0bf6c513e7e0
Kosmik <malpa.w.klapkach@gmail.com>
2022-11-12 10:16:13 +0100

    Extract copy camera movement

--- Assets/Scripts/CopyCamera.cs
+++ Assets/Scripts/CopyCamera.cs
@@ -1,79 +1,32 @@
 using UnityEngine;
 using NaughtyAttributes;
 using UnityEngine.Tilemaps;
-using System;
+
+public struct Photo
+{
+    public TileBase[] tiles;
+    public bool hasCollider; // not working 
+    public bool hasRididbody; // not working
+}
 
 public class CopyCamera : MonoBehaviour
 {
     [Header("To Link")]
     [SerializeField]
     private CopyCameraSettings settings;
+    public CopyCameraSettings Settings => settings;
+
     [SerializeField]
     private SpriteRenderer spriteRenderer;
+    public SpriteRenderer SpriteRenderer => spriteRenderer;
+
 
     [Header("States")]
-    [SerializeField, ReadOnly]
-    private Vector2Int intPosition;
-    
-    private Camera mainCamera;
-    
-    private bool isPressed;
-    private float holdTime;
+    [ReadOnly]
+    public Vector2Int intPosition;
 
     private void Start()
     {
-        Cursor.visible = false;
-        mainCamera = Camera.main;
         spriteRenderer.size = settings.ViewTileSize;
     }
-
-    private void Update()
-    {
-        var position = mainCamera.ScreenToWorldPoint(Input.mousePosition);
-        intPosition = new Vector2Int(
-            Mathf.RoundToInt(position.x),
-            Mathf.RoundToInt(position.y));
-        intPosition = ValidatePosition(intPosition);
-        transform.position = new Vector3(intPosition.x, intPosition.y, transform.position.z);
-
-        if (Input.GetMouseButtonDown(0))
-        {
-            holdTime = 0;
-            isPressed = true;
-        }
-        else if (Input.GetMouseButtonUp(0))
-        {
-            isPressed = false;
-        }
-
-        if (isPressed)
-        {
-            holdTime += Time.deltaTime;
-            if (holdTime >= settings.ActionHoldTime)
-            {
-                DoAction();
-            }
-        }
-    }
-
-    private void DoAction()
-    {
-        Debug.Log("TAKE PICTURE!");
-        isPressed = false;
-    }
-
-    private Vector2Int ValidatePosition(Vector2Int position)
-    {
-        int halfHorizontalMargin = (settings.WorldTileSize.x - settings.ViewTileSize.x) / 2;
-        int halfVerticalMargin = (settings.WorldTileSize.y - settings.ViewTileSize.y) / 2;
-
-        return new Vector2Int(
-            Mathf.Clamp(position.x, - halfHorizontalMargin, halfHorizontalMargin),
-            Mathf.Clamp(position.y, -halfVerticalMargin, halfVerticalMargin));
-    }
-
-    
-
-
-
 }
---
+++ Assets/Scripts/CopyCameraMovement.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(CopyCamera))]
+public class CopyCameraMovement : MonoBehaviour
+{
+    private CopyCamera copyCamera;
+    private Camera mainCamera;
+    private Vector2Int margin;
+
+    private void Awake()
+    {
+        copyCamera = GetComponent<CopyCamera>(); 
+        margin = new Vector2Int(
+            (copyCamera.Settings.WorldTileSize.x - copyCamera.Settings.ViewTileSize.x) / 2,
+            (copyCamera.Settings.WorldTileSize.y - copyCamera.Settings.ViewTileSize.y) / 2);
+    }
+
+
+    private void Start()
+    {
+        Cursor.visible = false;
+        mainCamera = Camera.main;
+    }
+
+    private void Update()
+    {
+        var position = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+        copyCamera.intPosition = new Vector2Int(
+            Mathf.RoundToInt(position.x),
+            Mathf.RoundToInt(position.y));
+        copyCamera.intPosition = GetValidatedPosition(copyCamera.intPosition);
+        transform.position = new Vector3(copyCamera.intPosition.x, copyCamera.intPosition.y, transform.position.z);
+    }
+
+    private Vector2Int GetValidatedPosition(Vector2Int position)
+    {
+        return new Vector2Int(
+            Mathf.Clamp(position.x, -margin.x, margin.x),
+            Mathf.Clamp(position.y, -margin.y, margin.y));
+    }
+}
