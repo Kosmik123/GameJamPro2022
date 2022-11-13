98e704eba24fc5193c5a6a3e3097a47b8ad958c8
Kosmik <malpa.w.klapkach@gmail.com>
2022-11-13 09:09:43 +0100

    Add music playing on last level

--- Assets/DontDestroy.cs
+++ Assets/DontDestroy.cs
@@ -1,11 +1,33 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class DontDestroy : MonoBehaviour
 {
+    private void Awake()
+    {
+        LastLevel.OnLastLevelAchieved += PlayFinalMusic;
+    }
+
+    private void PlayFinalMusic(AudioClip music)
+    {
+        if (TryGetComponent<AudioSource>(out var audioSource))
+        {
+            audioSource.Stop();
+            audioSource.clip = music;
+            audioSource.Play();
+        }
+    }
+
     void Start()
     {
         DontDestroyOnLoad(gameObject);
     }
+
+
+
+
+
+
 }
---
+++ Assets/Scripts/LastLevel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+
+public class LastLevel : MonoBehaviour
+{
+    public AudioClip finalMusic;
+
+    public static event Action<AudioClip> OnLastLevelAchieved;
+
+    private void Start()
+    {
+        OnLastLevelAchieved?.Invoke(finalMusic);
+    }
+}
